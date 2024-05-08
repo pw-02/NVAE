b9a35e087b73884609555c6c04a3797376848b93
pw-02 <watters.patrick@outlook.com>
2024-05-08 08:22:25 -0700

    batch sizw 32

--- train.py
+++ train.py
@@ -345,7 +345,7 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='C:\\Users\\pw\\projects\\NVAE\\data\\imagenet-lmdb',
                         help='location of the data corpus')
     # optimization
-    parser.add_argument('--batch_size', type=int, default=4,
+    parser.add_argument('--batch_size', type=int, default=32,
                         help='batch size per GPU')
     parser.add_argument('--learning_rate', type=float, default=1e-2,
                         help='init learning rate')
