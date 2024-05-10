8694aec9f9a098dbe7df1ac6330c095e53f38136
pw-02 <watters.patrick@outlook.com>
2024-05-10 12:40:01 -0700

    evaluate_is

--- evaluate.py
+++ evaluate.py
@@ -126,7 +126,7 @@ def main(eval_args):
                         # Convert the image from a tensor/array to a PIL Image
                         img_pil = Image.fromarray((img * 255).astype(np.uint8).transpose(1, 2, 0))
                         # Save as PNG (could be JPEG or another format if preferred)
-                        img_path = os.path.join("gen_images\\cifar10", f'image_{batch_num * batch_size + i + 1}.png')
+                        img_path = os.path.join("gen_images/cifar10", f'image_{batch_num * batch_size + i + 1}.png')
                         img_pil.save(img_path)
 
         end_time = time()
@@ -181,7 +181,7 @@ if __name__ == '__main__':
                         help='location of the checkpoint')
     parser.add_argument('--save', type=str, default='eval_results',
                         help='location of the checkpoint')
-    parser.add_argument('--eval_mode', type=str, default='evaluate_fid', choices=['sample', 'evaluate', 'evaluate_fid'],
+    parser.add_argument('--eval_mode', type=str, default='evaluate_is', choices=['sample', 'evaluate', 'evaluate_fid'],
                         help='evaluation mode. you can choose between sample or evaluate.')
     parser.add_argument('--eval_on_train', action='store_true', default=False,
                         help='Settings this to true will evaluate the model on training data.')
