ea91510677dd2be1e315a04b37aed4ccbb6487d7
pw-02 <watters.patrick@outlook.com>
2024-05-11 15:17:10 -0700

    exp-metfaces

--- train.py
+++ train.py
@@ -346,7 +346,7 @@ if __name__ == '__main__':
     parser.add_argument('--data', type=str, default='data/metfaces',
                         help='location of the data corpus')
     # optimization
-    parser.add_argument('--batch_size', type=int, default=32,
+    parser.add_argument('--batch_size', type=int, default=16,
                         help='batch size per GPU')
     parser.add_argument('--learning_rate', type=float, default=1e-2,
                         help='init learning rate')
@@ -361,7 +361,7 @@ if __name__ == '__main__':
     parser.add_argument('--weight_decay_norm_anneal', action='store_true', default=False,
                         help='This flag enables annealing the lambda coefficient from '
                              '--weight_decay_norm_init to --weight_decay_norm.')
-    parser.add_argument('--epochs', type=int, default=299,
+    parser.add_argument('--epochs', type=int, default=400,
                         help='num of training epochs')
     parser.add_argument('--warmup_epochs', type=int, default=5,
                         help='num of training epochs in which lr is warmed up')
